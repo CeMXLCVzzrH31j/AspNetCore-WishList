08783a101e689e063e3d3818c58a72185aab76b2
Steve Biz <mytbbucs@gmail.com>
2018-06-03 12:20:50 -0400

    Finished with Item Controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using System.Linq;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View("Create");
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.FirstOrDefault(r => r.Id == Id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
